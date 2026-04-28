ce6c29d970d818bd8bd9bb0984a7f71dc7a5c774
Surrender <3426075@gmail.com>
2026-04-29 01:48:49 +1000

    Update MainWindow.axaml.cs

--- 1x6Helper/Views/MainWindow.axaml.cs
+++ 1x6Helper/Views/MainWindow.axaml.cs
@@ -1,12 +1,19 @@
 using Avalonia.Controls;
+using System.IO;
 
 namespace _1x6Helper.Views
 {
     public partial class MainWindow : Window
     {
+        private const string SavePath = "imba_heroes.json";
         public MainWindow()
         {
+
             InitializeComponent();
+            if (!File.Exists(SavePath))
+            {
+                File.WriteAllText(SavePath, "{}");
+            }
         }
     }
 }
